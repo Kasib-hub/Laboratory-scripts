74e165823060f34e09158bb8355f43a7b02aa14d
Kasib <kasibzxc@gmail.com>
2022-04-05 00:17:12 -0400

    I'll call this ver 1.0. Tries to zip itself however

--- KasibScript.ps1
+++ KasibScript.ps1
@@ -1,47 +1,54 @@
 ﻿# Locates the previous month pqc and moves into new folder on USB
 # Zips those files then deletes originals
 # TODO: move AS files to AS folder, move LYSE and LYSENOWASH files, move CHARACTERIZATION (need its abbrev)
-# TODO: 
 
 $source = "C:\BD Export\Setup\Reports"
-$file = Get-ChildItem $source -Filter *pdf -Recurse
+$file = Get-ChildItem $source -Filter *pdf
+$ASdestination = "C:\BD Export\Setup\Reports\AS Reports"
 
 $prevmonth = (Get-Date).AddMonths(-1).ToString("MM")
 $year = (Get-Date).ToString("yyyy")
 $foldermonth = (Get-Date).ToString("MMM").ToUpper()
+$prevfoldermonth = (Get-Date).ToString("MMM").ToUpper()
 
-New-Item -Path "C:\Users\kasib\Documents\Work\Computer Science\Scripting\PQC $foldermonth" -ItemType Directory
+# create folder on USB
+New-Item -Path "D:\$year\PM PR 487131 $foldermonth $year" -ItemType Directory
 
-$destination = "C:\Users\kasib\Documents\Work\Computer Science\Scripting\PQC $foldermonth"
-$zip = "C:\Users\kasib\Documents\Work\Computer Science\Scripting\PQC $foldermonth\PQC $foldermonth.zip"
+$PQCdestination = "D:\$year\PM PR 487131 $foldermonth $year"
+$zip = "D:\$year\PM PR 487131 $foldermonth $year\PQC $prevfoldermonth $year.zip"
 
+# Move each PQC file in FLOW to USB, move each AS file to AS folder
 foreach ($i in $file) {
-    if ($i.Name -match $prevmonth -and $i.Name -match $year) {
-        Move-Item $i $destination
+    if ($i.Name -match $prevmonth -and $i.Name -match "PQC") {
+        Move-Item $i.FullName $PQCdestination
+    }
+    if ($i.Name -match "AS" -and $i.Name -match $year) {
+        Move-Item $i.FullName $ASdestination
     }
 }
 
-Compress-Archive $destination\* $zip
+# zip the moved files in USB
+Add-Type -Assembly 'System.IO.Compression.Filesystem'
+[System.IO.Compression.ZipFile]::CreateFromDirectory($PQCdestination, $zip)
 
-#once the item is selected just pipe into your action
-Get-ChildItem $destination -Filter *.pdf -Recurse | Remove-Item -Force
+foreach ($i in $file) {
+    if ($i.Name -match "Lyse" -or $i.Name -match "LYSE" -or $i.Name -match "lyse") {
+        Move-Item $i.FullName $PQCdestination
+    }
+}
 
-for ($counter = 1; $counter -le 10; $counter++) {
+# Not using the delete function right now
+# Get-ChildItem $PQCdestination -Filter *.pdf | Remove-Item -Force
+
+Write-Host `n`n`n`n
+for ($counter = 1; $counter -le 100; $counter++) {
     Write-Progress -Activity "Update Progress" -Status "$counter% Complete:" -PercentComplete $counter;
 }
 
-
-Write-Host "`n`n`n`n`n`nCompleted"
-
-for ($seconds=5; $seconds -gt -1; $seconds--) {
-    
-    Write-Host -NoNewline ("`rseconds remaining: " + ("{0:d4}" -f $seconds))
-    Start-Sleep -Seconds 1
-}
 
 # FLOW reports @ C:\BD Export\Setup\Reports
 # USB @ D:\
 # PQC destination D:\2022\PM PR 487131 MAR 2022 contains zip of PQC FEB 2022.zip, LyseNoWash03072022.pdf and lyse wash
 # AS FILE destination C:\BD Export\Setup\Reports\AS Reports
 #
-#
+# https://theposhwolf.com/howtos/PowerShell-and-Zip-Files/
